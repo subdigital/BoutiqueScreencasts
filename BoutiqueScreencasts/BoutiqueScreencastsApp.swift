0f719d24874da7c29b4e910d20ba7141049a7350
Ben Scheirman <ben@scheirman.com>
2022-06-25 12:42:31 -0500

    Initial Commit

---
+++ BoutiqueScreencasts/BoutiqueScreencastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoutiqueScreencastsApp.swift
+//  BoutiqueScreencasts
+//
+//  Created by Ben Scheirman on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct BoutiqueScreencastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
